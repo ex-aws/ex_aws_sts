13642c8b6afab51c58de0d35b86ba1260bd23d6a
Ben Wilson <benwilson512@gmail.com>
2018-12-28 17:44:01 -0500

    version bumpg

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAws.STS.Mixfile do
   def project do
     [
       app: :ex_aws_sts,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
