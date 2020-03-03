f65f3f69be3ea3f431e74b08cb8608a3b1956f31
Tiago Sousa <tiago.joao@gmail.com>
2020-03-03 09:51:22 +0000

    Fix issue caused by changing key order in map

--- lib/ex_aws/sts/auth_cache/assume_role_web_identity_adapter.ex
+++ lib/ex_aws/sts/auth_cache/assume_role_web_identity_adapter.ex
@@ -55,11 +55,11 @@ defmodule ExAws.STS.AuthCache.AssumeRoleWebIdentityAdapter do
     %{
       role_arn: env_role_arn(config),
       role_session_name: role_session_name(config),
-      web_identity_token: web_identity_token(config)
+      web_identity_token: web_identity_token(config),
       # necessary for now due to how ExAws.request() works
       access_key_id: "dummy",
       # necessary for now due to how ExAws.request() works
-      secret_access_key: "dummy",
+      secret_access_key: "dummy"
     }
   end
 
